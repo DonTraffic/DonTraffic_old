f29508cbb62a6f66a591f2db9193a876b4899b3e
Don <dogerfantom6@gmail.com>
2020-12-09 20:31:51 +0300

    +

--- website/JulinsWeb/script/script.js
+++ website/JulinsWeb/script/script.js
@@ -17,12 +17,4 @@ function heightValue() {
 function heigthRegistr() {
   $('.registration').css('height', height);
 
-  if (height > 425) {
-    $('.enterRegistrPC').css('display','flex');
-    $('.enterRegistrMob').css('display','none');
-  } else {
-    $('.enterRegistrPC').css('display','none');
-    $('.enterRegistrMob').css('display','flex');
-  }
-
 }
