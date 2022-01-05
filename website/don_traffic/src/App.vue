9110cd6c5f55ec6c53125e44c36c9675848c1eff
DonTraffic <dogerfantom6@gmail.com>
2022-01-06 01:50:05 +0300

    add start window

--- website/don_traffic/src/App.vue
+++ website/don_traffic/src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="main">
     <StartWindow />
+    <div class="container main_grid">
+      <div class="main_person">
+        <img src="./assets/persona.png" alt="person">
+      </div>
+      <div class="main_content">
+        <div class="main_content-prof">
+          Привет, я
+          <div class="content_prof-container">Web-программист</div>
+        </div>
+        <div class="main_content-name">
+          Меня зовут Уханов Дмитрий
+        </div>
+        <button class="main_content-button">Почему именно я?</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,22 +33,83 @@ export default {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
-    -ms-user-select: none;
-    -moz-user-select: none;
-    -khtml-user-select: none;
-    -webkit-user-select: none;
+    user-select: none;
 
   }
 </style>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+.container{
+  // width: 100vw;
+  margin: 0px auto;
+  max-width: 1366px;
+  width: 100%;
+  transition: .4s ease;
+  // background-color: aqua;
+}
+
 .main {
   position: relative;
   width: 100vw;
   min-height: 100vh;
-  background-color: rgb(59, 59, 59);
+  background-color: #000;
   margin: 0 auto;
   overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .main_grid{
+    display: grid;
+    grid-template-columns: 1fr 1.5fr;
+    color: #fff;
+
+    .main_person{
+      img{
+        width: 100%;
+      }
+    }
+
+    .main_content{
+      .main_content-prof{
+        font-size: 31.25px;
+        .content_prof-container{
+          display: inline-block;
+          color: #000;
+          background: #FF7514;
+          border: 1px solid #000;
+          box-sizing: border-box;
+          border-radius: 4px;
+          padding: 4px;
+        }
+      }
+      .main_content-name{
+        font-size: 31.25px;
+      }
+      .main_content-button{
+        font-size: 20px;
+        color: #FF7514;
+        background: #000;
+        border: 1px solid #FF7514;
+        box-sizing: border-box;
+        border-radius: 6px;
+        padding: 8px 16px;
+        transition: .8s ease;
+
+        &:hover{
+          color: #000;
+          background: #FF7514;
+          border: 1px solid #000;
+          cursor: pointer;
+          transition: .4s ease;
+        }
+
+      }
+
+    }
+
+  }
+
 }
 </style>
--- website/don_traffic/src/components/startWindow.vue
+++ website/don_traffic/src/components/startWindow.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="startWindow_container">
-    <h1>DonTraffic.ru</h1>
-    <p>Если программировать, то с любовью</p>
-    <button @click="windowMove()">Перейти к портфолио</button>
+
+    <div id="startWindow_screensaver"></div>
+
+    <div id="startWindow_heading" class="textDropped">
+      <h1 class="textDropped-top">DonTraffic.ru</h1>
+      <div class="textDropped-bottom">唐特拉菲克</div>
+    </div>
+    <div id="startWindow_subtitle" class="textDropped">
+      <h2 class="textDropped-top">Если программировать, то с любовью</h2>
+      <div class="textDropped-bottom">如果你编程，那么用爱</div>
+    </div>
+    <button id="startWindow_button" class="textDropped" @click="windowMove()">
+      <h2 class="textDropped-top">Перейти к портфолио</h2>
+      <div class="textDropped-bottom">进入投资组合</div>
+    </button>
+    <div class="startWindow_eggsMongen" v-on:click="eggsMongen()">
+      <img src="./../assets/startWindow/eggsMongen.png" alt="ebalo">
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
   name: 'startWindow',
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    setTimeout(this.funStartAnimation, 3000)
+    setTimeout(this.funScreensaverOf, 3000)
+  },
   methods: {
+    // выключение наложенной заставки
+    funScreensaverOf(){
+      $('#startWindow_screensaver').css('display', 'none')
+    },
+    // старт анимации текста
+    funStartAnimation(){
+      let items = document.querySelectorAll('.textDropped')
+      for (let i = 0; i < items.length; i++) {
+        function useAnim() {
+          $(items[i].querySelector('.textDropped-top')).addClass('activeAnimTop')
+          $(items[i].querySelector('.textDropped-bottom')).addClass('activeAnimBottom')
+        }
+        setTimeout(useAnim, i * 2000)
+      }
+    },
+    // при клике на кнопку, поднимаем стартовый экран
     windowMove(){
       document.getElementById('startWindow_container').classList.toggle('active')
+    },
+
+    eggsMongen(){
+      console.log('mongen eggs')
     },
   }
 }
@@ -22,57 +68,149 @@ export default {
     position: absolute;
     width: 100vw;
     height: 100vh;
+    transition: transform 1s;
+    overflow: hidden;
+
     background-color: rgb(129, 129, 129);
     background: url('../assets/startWindow/startWindowBackground.jpg') center no-repeat;
     background-size: cover;
+
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    transition: transform 1s;
 
-    h1{
-      background: linear-gradient(90deg, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0.5) 100%);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      font-style: normal;
-      font-weight: normal;
-      font-size: 61.04px;
+    #startWindow_screensaver{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgb(0, 0, 0);
+      z-index: 10;
+      animation: screensaverOf 3s ease;
+      animation-fill-mode: forwards;
+    }
+
+    @keyframes screensaverOf {
+      to{
+        opacity: 0;
+      }
+    }
+
+    #startWindow_heading{
+      width: 405px;
+
+      h1, div{
+        background: linear-gradient(90deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0.6) 100%);
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 61.04px;
+        text-align: center;
+        width: 100%;
+      }
+
+      div{
+        font-size: 50.04px;
+      }
 
     }
 
-    p{
-      background: linear-gradient(90deg, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0.7) 50%, rgba(255, 255, 255, 0.4) 100%);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      font-family: Aksara Bali Galang;
-      text-align: center;
-      font-size: 16px;
-      line-height: 24px;
-      margin: 0px 0px 32px 0px;
-      max-width: 170px;
-      display: inline-block;
+    #startWindow_subtitle{
+      margin-bottom: 32px;
+      max-width: 230px;
+      width: 220px;
+      height: 48px;
+
+      h2, div{
+        font-size: 16px;
+        opacity: 0.5;
+        color: white;
+        text-align: center;
+        line-height: 24px;
+        font-weight: 300;
+      }
       
     }
 
-    button{
+    #startWindow_button{
+      height: 48px;
+      width: 212px;
       display: flex;
       justify-content: center;
       align-items: center;
-      padding: 8px 16px;
       background: rgba(255, 255, 255, 0.1);
       border-radius: 3px;
-      border: 1px solid rgba(255, 255, 255, 0.7);
-      font-size: 16px;
+      border: 1px solid transparent;
+      border-image: linear-gradient(#ffffff , #ffffff81, #ffffff00);
+      border-image-slice: 1;
       color: rgba(255, 255, 255, 0.7);
       transition: .5s ease;
+
+      h2, div{
+        font-size: 16px;
+        color: #FFF;
+        font-weight: 100;
+        height: 100%;
+        display: flex;
+        align-items: center;
+      }
 
       &:hover{
         cursor: pointer;
         background: rgba(255, 255, 255, 0.15);
-        border: 1px solid rgba(255, 255, 255, 1);
+        border-image: linear-gradient(#ffffff , #ffffff, #ffffff);
+        border-image-slice: 1;
         color: rgba(255, 255, 255, 1);
         transition: .3s ease;
+      }
+
+    }
+
+    .textDropped{
+      position: relative;
+
+      .textDropped-top{
+        position: absolute;
+        top: -60%;
+        transform: rotateX(90deg);
+      }
+
+      .textDropped-bottom{
+        transform: rotateX(0deg);
+      }
+
+      .activeAnimTop{
+        animation: animTextDroppedTop 3s ease-in-out;
+        animation-fill-mode: forwards;
+      }
+
+      .activeAnimBottom{
+        animation: animTextDroppedBottom 3s ease-in-out;
+        animation-fill-mode: forwards;
+      }
+      
+    }
+
+    .startWindow_eggsMongen{
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      overflow: hidden;
+      height: 100px;
+      img{
+        height: 100%;
+        transform: translateX(-105px);
+        // transform: translateY(5px);
+        transition: .4s ease;
+        border-top-right-radius: 20px;
+      }
+
+      &:hover{
+        cursor: pointer;
+        img{
+          transform: translateX(0px);
+        }
       }
 
     }
@@ -82,4 +220,19 @@ export default {
   .active{
     transform: translateY(-100%);
   }
+
+  @keyframes animTextDroppedTop {
+    to{
+      transform: rotateX(0deg);
+      top: 0px;
+    }
+  }
+
+  @keyframes animTextDroppedBottom {
+    to{
+      // transform: rotateX(90deg);
+      opacity: 0;
+    }
+  }
+
 </style>
